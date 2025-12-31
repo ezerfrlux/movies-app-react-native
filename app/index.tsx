ebbcb23af82523899a4265f1a9047e648afcd24c
ezerfrlux <aaron.salinas318@gmail.com>
2025-12-30 18:18:08 -0600

    [UPDATE] -  Uploading enviroments variables

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="bg-red-400">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-3xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
