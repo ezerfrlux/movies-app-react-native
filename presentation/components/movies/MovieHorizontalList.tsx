55fc4735c53d803e8faf8b37a4e61a45fc17fb33
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-01 12:20:47 -0600

    [FEATURE] -  New feature to load new movies on scroll was near to finish

--- presentation/components/movies/MovieHorizontalList.tsx
+++ presentation/components/movies/MovieHorizontalList.tsx
@@ -1,6 +1,6 @@
 import { Movie } from "@/infrastructure/interfaces/moviedb-response";
-import React from "react";
-import { FlatList, Text, View } from "react-native";
+import React, { useRef } from "react";
+import { FlatList, NativeScrollEvent, NativeSyntheticEvent, Text, View } from "react-native";
 import MoviePoster from "./MoviePoster";
 
 interface Props {
@@ -11,6 +11,22 @@ interface Props {
 
 const MovieHorizontalList = ({ movies, title, className }: Props) => {
   
+  const isLoading = useRef(false)
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (isLoading.current) return
+
+    const {contentOffset, layoutMeasurement,contentSize} = event.nativeEvent
+
+    const isEndReached = (contentOffset.x + layoutMeasurement.width + 600) >= contentSize.width
+
+    if (!isEndReached) return
+
+    isLoading.current = true
+    console.log("cargando peliculas");
+    
+  }
+
   return (
     <View className={`${className}`}>
       {title && <Text className="px-4 mt-2 text-3xl font-bold">{title}</Text>}
@@ -20,6 +36,7 @@ const MovieHorizontalList = ({ movies, title, className }: Props) => {
         showsHorizontalScrollIndicator={false}
         keyExtractor={(item) => `${item.id}`}
         renderItem={({item}) => <MoviePoster id={item.id} poster={item.poster_path} smallPoster/>} 
+        onScroll={onScroll}
       /> 
     </View>
   );
