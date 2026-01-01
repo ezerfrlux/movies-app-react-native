873a5a324ff5d1024d1b2a9104abac512995770f
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-01 15:36:25 -0600

    [UI] -  Gradient was added with the Library Linear Grandient from Expo

--- presentation/components/movie/MovieHeader.tsx
+++ presentation/components/movie/MovieHeader.tsx
@@ -1,43 +1,71 @@
-import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import React from "react";
-import { Image, Pressable, Text, useWindowDimensions, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  Text,
+  useWindowDimensions,
+  View,
+} from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
 interface Props {
-  poster:string;
+  poster: string;
   originalTitle: string;
   title: string;
 }
 
-const MovieHeader = ({poster, originalTitle, title}: Props) => {
-  const { height } = useWindowDimensions();
-  
+const MovieHeader = ({ poster, originalTitle, title }: Props) => {
+  const { height: screenHeight } = useWindowDimensions();
+
   return (
     <>
-
-    <View style={{
-      position:'absolute',
-      zIndex: 99,
-      elevation:9,
-      top:35,
-      left:10
-    }}>
-      <Pressable onPress={() => router.dismiss()}>
-        <Ionicons   name="arrow-back" size={30} color={"white"} className="shadow"/>
-      </Pressable>
-    </View>
+      <LinearGradient
+        colors={["rgba(0,0,0,0.1)", "transparent"]}
+        start={[0, 0]}
+        style={{
+          height: screenHeight * 0.4,
+          position: "absolute",
+          zIndex: 1,
+          width: "100%",
+        }}
+      />
+      <View
+        style={{
+          position: "absolute",
+          zIndex: 99,
+          elevation: 9,
+          top: 35,
+          left: 10,
+        }}
+      >
+        <Pressable onPress={() => router.dismiss()}>
+          <Ionicons
+            name="arrow-back"
+            size={30}
+            color={"white"}
+            className="shadow"
+          />
+        </Pressable>
+      </View>
 
       <View
-        style={{ height: height * 0.7 }}
+        style={{ height: screenHeight * 0.7 }}
         className="shadow-xl shadow-black/20"
       >
         <View className="flex-1 rounded-b-[25px] overflow-hidden">
-          <Image source={{uri: poster}} resizeMode="cover"  className="flex-1" />
+          <Image
+            source={{ uri: poster }}
+            resizeMode="cover"
+            className="flex-1"
+          />
         </View>
 
         <View className="px-5 mt-5">
           <Text className="font-normal">{originalTitle}</Text>
-          <Text className="text-2xl font-semibold">{title }</Text>
+          <Text className="text-2xl font-semibold">{title}</Text>
         </View>
       </View>
     </>
